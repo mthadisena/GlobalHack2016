66e8e5f8972be8ba9dd74843caa114ccaec16f8e
wats1109 <chandler.watson@usablelife.com>
2016-10-21 19:35:30 -0500

    adding mvc template

---
+++ HomelessHelper/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace HomelessHelper
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
