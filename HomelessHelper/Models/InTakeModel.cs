5d087b4dc60f975c424ecc4d2161057f6b49de7c
mtblanton <mtblanton@gmail.com>
2016-10-22 10:36:24 -0500

    added things to intake model

--- HomelessHelper/Models/InTakeModel.cs
+++ HomelessHelper/Models/InTakeModel.cs
@@ -7,8 +7,56 @@ namespace HomelessHelper.Models
 {
     public class InTakeModel
     {
-       public string FirstName { get; set; }
-       public string LastName { get; set; }
-       public string SSN { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SSN { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Race Race { get; set; }
+        public bool Gender { get; set; }
+        public VetStatus VetStatus { get; set; }
+
     }
+}
+
+public enum Race
+{
+    AmericanIndian,
+    Asian,
+    Black,
+    HawaiiSouthPacificIslander,
+    White,
+    Other
+}
+
+public class VetStatus
+{
+    public DateTime YearEnteredService { get; set; }
+    public DateTime YearLeftService { get; set; }
+    
+    
+}
+
+public enum WarServedIn
+{
+    WorldWar2,
+    KoreanWar,
+    VietnamWar,
+    DesertStorm,
+    AfghanistanOEF,
+    IraqOIF,
+    OtherTheater
+}
+
+public enum MilitaryBranch
+{
+    Army,
+    AirForce,
+    Marines,
+    Navy
+}
+
+public enum DischargeStatus
+{
+    Honorable,
+    Dishonorable
 }
