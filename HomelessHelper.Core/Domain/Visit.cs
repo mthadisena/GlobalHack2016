aff7bcf76594525f28316babd7f6e5d041829e8b
wats1109 <chandler.watson@usablelife.com>
2016-10-22 10:43:48 -0500

    removing obj crap, visit

---
+++ HomelessHelper.Core/Domain/Visit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomelessHelper.Core.Domain
+{
+    public class Visit
+    {
+        public Shelter Shelter { get; set; }
+        public Client Client { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+    }
+}
