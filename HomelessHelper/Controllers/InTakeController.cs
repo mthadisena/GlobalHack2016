ef4ed0379ac74e9f3f7591eaa1971c59e19d3d14
verg0630 <jvergara@lsvusa.com>
2016-10-22 10:07:29 -0500

    creating intake model to get started

--- HomelessHelper/Controllers/InTakeController.cs
+++ HomelessHelper/Controllers/InTakeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using HomelessHelper.Models;
 
 namespace HomelessHelper.Controllers
 {
@@ -6,7 +7,7 @@ namespace HomelessHelper.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View(new InTakeModel());
         }
     }
 }
