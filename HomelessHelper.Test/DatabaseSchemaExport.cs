002367a31f2058f1166a7eb182c7dffd426149c6
wats1109 <chandler.watson@usablelife.com>
2016-10-22 11:27:02 -0500

    adding staging method in DSE

--- HomelessHelper.Test/DatabaseSchemaExport.cs
+++ HomelessHelper.Test/DatabaseSchemaExport.cs
@@ -1,4 +1,5 @@
-﻿using HomelessHelper.Core.EntityFramework;
+﻿using HomelessHelper.Core.Domain;
+using HomelessHelper.Core.EntityFramework;
 using NUnit.Framework;
 
 namespace HomelessHelper.Test
@@ -6,11 +7,25 @@ namespace HomelessHelper.Test
     [TestFixture]
     public class DatabaseSchemaExport
     {
+        private HomelessHelperDbContext _context = new HomelessHelperDbContext();
+
         [Test]
         public void Export()
         {
-            var manager = new DatabaseManager(new HomelessHelperDbContext());
+            var manager = new DatabaseManager(_context);
             manager.DropAndCreate();
+        }
+
+        [Test]
+        public void Stage()
+        {
+            //example
+            var shelter = new Shelter
+            {
+                Name = "Really cool shelter"
+            };
+            _context.Shelters.Add(shelter);
+            _context.SaveChanges();
         }
     }
 }
