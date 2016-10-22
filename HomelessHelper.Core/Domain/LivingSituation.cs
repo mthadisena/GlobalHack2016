2ff277d4e5e1cccbb75398a3a1756db983446f8d
Ashley Lawrence <Ashley.Lawrence@usablelife.com>
2016-10-22 11:30:47 -0500

    working on adding living situation table

---
+++ HomelessHelper.Core/Domain/LivingSituation.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using HomelessHelper.Core.EntityFramework;
+
+namespace HomelessHelper.Core.Domain
+{
+    [Table("LivingSituation")]
+    public class LivingSituation : Entity
+    {
+        
+    }
+}
