a80f44459ae9b6b178ed8243499cdd5995ffb486
verg0630 <jvergara@lsvusa.com>
2016-10-22 11:15:01 -0500

    add booking on context

--- HomelessHelper.Core/EntityFramework/HomelessHelperDbContext.cs
+++ HomelessHelper.Core/EntityFramework/HomelessHelperDbContext.cs
@@ -11,6 +11,7 @@ namespace HomelessHelper.Core.EntityFramework
         public DbSet<ServicePerson> ServicePersons { get; set; }
         public DbSet<Client> Clients { get; set; }
         public DbSet<Bed> Beds { get; set; }
+        public DbSet<BedBooking> BedBookings { get; set; }
         public DbSet<Address> Addresses { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
