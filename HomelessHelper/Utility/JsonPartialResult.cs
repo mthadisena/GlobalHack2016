6879e0798584cfd611813e5ca83307e36019da4b
Mahesh <mthadisena@lsvusa.com>
2016-10-22 13:10:44 -0500

    added json partial Result object to return status and message

---
+++ HomelessHelper/Utility/JsonPartialResult.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace HomelessHelper.Utility
+{
+    public class JsonPartialResult
+    {
+        public JsonResultStatus Status { get; set; }
+        public string Message { get; set; }
+        
+        public string Html { get; set; }
+
+        public object Model { get; set; }
+    }
+
+    public enum JsonResultStatus
+    {
+        Successful = 0,
+        Warning = 1,
+        Error = 2,
+    }
+}
