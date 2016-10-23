1a09cf054f33d9b75836e55d98d8a155f33a0dab
Mahesh <mthadisena@lsvusa.com>
2016-10-23 03:09:48 -0500

    Added Shelter Details with Clients associated with it

---
+++ HomelessHelper.Test/Shelter/ShelterRepositorySpecs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HomelessHelper.Core.Domain;
+using HomelessHelper.Core.EntityFramework;
+using NUnit.Framework;
+
+namespace HomelessHelper.Test.Shelter
+{
+    public class ShelterRepositorySpecs
+    {
+        [TestFixture]
+        public class ShouldReturnShelterDetails
+        {
+            private HomelessHelperDbContext _dbContext;
+
+            [SetUp]
+            public void Setup()
+            {
+                _dbContext = new HomelessHelperDbContext();
+            }
+
+            [Test]
+            public void ShoudReturnShelterAndClientsAssociatedWithIt()
+            {
+                var guid = new Guid("4D1D68F4-D8CB-427C-986B-0DCC995F7964");
+                var bedsInShelter = _dbContext.Beds.Where(x => x.Shelter.Id == guid).ToList();
+
+                var aaa = _dbContext.BedBookings.Where(x => x.Bed.Shelter.Id == guid).ToList();
+
+                var allClients = new List<Client>();
+
+                bedsInShelter.ForEach(x =>
+                {
+                    var clientIds =_dbContext.BedBookings.Where(b => b.Bed.Id == x.Id).Select(y => y.ClientId).ToList();
+
+                    allClients.AddRange(_dbContext.Clients.Where(c => clientIds.Contains(c.Id)).ToList());
+                });
+
+                var count = allClients.Count;
+            }
+        }
+    }
+}
