544ec92f23b3f0ebdb87971e224df21df5fe0ff7
Ashley Lawrence <Ashley.Lawrence@usablelife.com>
2016-10-23 01:31:52 -0500

    working on check out

---
+++ HomelessHelper/Models/CheckOutModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomelessHelper.Models
+{
+    public class CheckOutModel
+    {
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string CheckOutSummary { get; set; }
+
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public int NumberOfDaysStayed => GetNumberOfDays();
+
+        public int GetNumberOfDays()
+        {
+            var diff = CheckOutDate - CheckInDate;
+            return (int)diff.TotalDays;
+        }
+    }
+}
