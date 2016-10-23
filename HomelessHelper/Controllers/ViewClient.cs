c7f66e3b3c7ba3cf79a9a6bdca6e0543ed4b3eee
wats1109 <chandler.watson@usablelife.com>
2016-10-23 01:13:15 -0500

    more client stuff

---
+++ HomelessHelper/Controllers/ViewClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HomelessHelper.Core.Domain;
+using HomelessHelper.Core.EntityFramework;
+
+namespace HomelessHelper.Controllers
+{
+    public class ViewClientController : Controller
+    {
+        public ActionResult Index(Guid id)
+        {           
+            return View(new GetClientQuery().Query(id));
+        }
+    }
+
+    public class GetClientQuery
+    {
+        private readonly HomelessHelperDbContext _context = new HomelessHelperDbContext();
+        public Client Query(Guid id)
+        {
+            return _context.Clients.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
